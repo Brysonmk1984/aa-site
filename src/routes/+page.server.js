a53066f4377c781f9e079cf19e8f2ec2a9dad87e
Bryson Kruk <brysonmk1984@gmail.com>
2023-09-17 11:51:48 -0600

    init commit aa-site

---
+++ src/routes/+page.server.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('@sveltejs/kit').Load}
+ */
+export async function load(ctx){
+  const { fetch, locals, params, platform, getClientAddress, route, url, request } = ctx;
+
+  const apiEndpoint = "http://127.0.0.1:8111";
+
+  try {
+    const response = await fetch(apiEndpoint);
+    const armies = await response.json();
+  
+    return { armies };
+  } catch (error) {
+    console.error(error);
+    
+  }
+
+  
+  return {
+    props: {
+      origin: url.origin
+    }
+  };
+}
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let data;
+  export const armies = data.armies;
+  console.log({armies})
+</script>
+
+<section>
+  <h1>Armies of Avalore</h1>
+  <p>An incremental strategy game set in a realm of medieval fantasy</p>
+  <p>Battle for territory and control the Avalore! </p>
+  <ul>
+    { #if !!armies && armies.length }
+      {#each armies as army}
+        <li>{army.name}</li>
+      {/each}
+    { /if }
+  </ul>
+</section>
+
+<style>
+  :global(body) {
+    background-color: black;
+  }
+  h1 {
+    color: darkgray;
+  }
+  p {
+    color: darkred;
+  }
+  li{
+    color: antiquewhite;
+  }
+</style>
+
